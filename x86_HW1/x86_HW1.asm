dfe4d580a564682051b304146dbad482d2308060
JongHoon <jhshin305@gmail.com>
2025-05-04 02:07:49 +0900

    final

--- x86_HW1/x86_HW1.asm
+++ x86_HW1/x86_HW1.asm
@@ -86,20 +86,87 @@ sector_2:						; Program Starts
 ; Print your Name in VMware screen											    ;
 ; Print your ID in VMware screen											    ;
 ; Print the value(word size) in the Stack Pointer after executing the above code;
-;																				;
-;																				;
-;																				;
-;																				;
-;																				;
-;																				;
+;																	    		;
+;																	    		;
+;																	    		;
+;																	    		;
+;																	    		;
+;																	    		;
 ;-------------------------------------------------------------------------------;
+print_string:
+	mov cx, 0	;row number
+	mov di, 0	;column number
 
+	mov esi, ID
+	call print_string_loop
+	mov esi, NAMEE
+	call new_line
+	call print_string_loop
+	mov esi, Answer
+	call new_line
+	call print_string_loop
+	call print_sp
+	jmp exit
 
+print_string_loop:
+	lodsb             ; DS:SI의 데이터를 AL로 로드하고 SI 증가
+	cmp al, 0         ; NULL이면 문자열 끝
+	je done_print
+	mov byte [es:di], al   ; 문자 출력
+	inc di
+	mov byte [es:di], 0x07  ; 속성(흰색 글자, 검정 배경)
+	inc di
+	jmp print_string_loop
+
+done_print:
+	ret
+
+new_line:
+	inc cx
+	mov ax, cx
+	mov bx, 160
+	mul bx
+	mov di, ax
+	ret
+print_sp:
+	pop bx
+	push cx
+	mov cx, 0	;count
+print_sp_loop:
+	mov ax, 0xF000
+	and ax, bx
+	shr ax, 12
+	cmp al, 0xA
+	jae print_alphabet
+	jmp print_number
+print_alphabet:
+	add al, 0x37
+	jmp print_sp_loop2
+print_number:
+	add al, 0x30
+	jmp print_sp_loop2
+
+print_sp_loop2:
+	mov byte [es:di], al   ; 문자 출력
+	inc di
+	mov byte [es:di], 0x07  ; 속성(흰색 글자, 검정 배경)
+	inc di
+	shl bx, 4
+	inc cx
+	cmp cx, 4
+	je done_print_sp
+	jmp print_sp_loop
+
+done_print_sp:
+	pop cx
+	ret
+
+exit:
 
 ;---------------------- Write your Name and ID here-----------------------------;
 
-ID  db 'ID : 2015000000',0
-NAMEE db 'NAME : Hong Kil Dong',0
+ID  db 'ID : 2020311573',0
+NAMEE db 'NAME : Shin Jong Hoon',0
 Answer db 'A value in Stack Pointer(word size) : ',0
 
 ;-------------------------------------------------------------------------------;
